4d7aed479fbeb47befc347abb354df715974f036
Fraser Watson <fraser.watson@mac.com>
2024-02-14 15:38:44 +0000

    Multiple choice answers can now be selected

--- src/components/enduserformcomponents.js
+++ src/components/enduserformcomponents.js
@@ -11,14 +11,54 @@ export function AnswerableQuestionHeader({questionNumber, required}) {
 export function AnswerableSimpleQuestion({itemData, updateState}) {
     return <div>
         <p>Answer</p><br />
-        <input type="text" placeholder="Enter an answer" value={itemData.providedAnswer != undefined ? itemData.providedAnswer : ""} /><br />
+        <input type="text" placeholder="Enter an answer" value={itemData.providedAnswer != undefined ? itemData.providedAnswer : ""} onChange={(event) => {
+            let newItemData = Object.assign({}, itemData);
+            newItemData.providedAnswer = event.target.value;
+
+            updateState(itemData, newItemData);
+        }} /><br />
     </div>;
 }
 
 export function AnswerableMultipleQuestion({itemData, updateState}) {
+    function isAnswerChosen(index) {
+        if (itemData.multipleAllowed) {
+            return itemData.providedAnswers != undefined && itemData.providedAnswers.indexOf(index) != -1;
+
+        } else {
+            return itemData.providedAnswer == index;
+        }
+    }
+
     return (<>
     {itemData.options.map((item, i) => {
-        return <><label>{item}</label><input type={itemData.multipleAllowed ? "checkbox" : "radio"} /><br /></>;
+        return <><label>{item}</label><input type={itemData.multipleAllowed ? "checkbox" : "radio"} checked={isAnswerChosen(i)} onChange={(event) => {
+            let newItemData = Object.assign({}, itemData);
+            if (itemData.multipleAllowed) {
+
+                if (event.target.checked) {
+                    let newAnswers = newItemData.providedAnswers != undefined ? [...newItemData.providedAnswers] : [];
+                    newAnswers.push(i);
+                    newItemData.providedAnswers = newAnswers;
+
+                } else {
+                    newItemData.providedAnswers = itemData.providedAnswers.filter((answer) => answer != i);
+                    if (newItemData.providedAnswers.length == 0) {
+                        delete newItemData.providedAnswers;
+                    }
+                }
+
+            } else {
+                if (event.target.checked) {
+                    newItemData.providedAnswer = i;
+
+                } else {
+                    delete newItemData.providedAnswer;
+                }
+            }
+
+            updateState(itemData, newItemData);
+        }} /><br /></>;
     })}</>);
 }
 
@@ -27,11 +67,11 @@ export function AnswerableQuestion({itemData, questionNumber, updateState}) {
 
     switch (itemData.type) {
         case "single":
-            questionBody = <AnswerableSimpleQuestion itemData={itemData} updateState={useState} />;
+            questionBody = <AnswerableSimpleQuestion itemData={itemData} updateState={updateState} />;
             break;
 
         case "multiple":
-            questionBody = <AnswerableMultipleQuestion itemData={itemData} updateState={useState} />;
+            questionBody = <AnswerableMultipleQuestion itemData={itemData} updateState={updateState} />;
             break;
 
         default:
@@ -52,9 +92,13 @@ export function AnswerableQuestionHolder() {
         {type: "multiple", question: "Select one", required: true, options: ["A", "B", "C", "D"], multipleAllowed: true}
     ]);
 
+    function updateState(oldEntry, newEntry) {
+        setAnswerableQuestionList(answerableQuestionList.map(entry => entry == oldEntry ? newEntry : entry));
+    }
+
     return (<div>
         {answerableQuestionList.map((question, i) => {
-            return <AnswerableQuestion itemData={question} questionNumber={i} updateState={null} />
+            return <AnswerableQuestion itemData={question} questionNumber={i} updateState={updateState} />
         })}
     </div>);
 }
